7f9481057f099630db62aded3c808ec9e0523ec0
jousethen <jthen93@gmail.com>
2021-08-11 01:01:12 -0400

    fetching toys. Just need to iterate through toyCollection to make cards

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const toyCollection = document.querySelector("#toy-collection");
+
+  fetch("http://localhost:3000/toys")
+    .then(function (response) {
+      return response.json()
+    }).then(function (json) {
+
+      //Cycle through array of objects
+      console.log(json);
+      let card = document.createElement("div");
+      card.class = "card";
+      toyCollection.appendChild(card);
+    });
+
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
